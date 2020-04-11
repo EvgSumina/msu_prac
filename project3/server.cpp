b3b4e2fe9171e9aae27a815c3368cea131453a1c
EvgSumina <eugenia.sumina@yandex.ru>
2020-04-11 21:36:09 +0300

    New change of server

--- project3/server.cpp
+++ project3/server.cpp
@@ -18,32 +18,126 @@ using namespace std;
 #define FNFSTR "404 Error File Not Found"
 #define BRSTR "Bad Request"
 
+
 char buffer[] = "HTTP/1.1 200 Ok\nContent-Type: text/html";
 
-vector <string> CreatVect(char *str, int Count)
-{
-	int i = 0, k, is, j;
-	vector <string> ivector;
-	string x;
 
-	while(i != (Count + 1))
+struct word
+{
+	char *str;
+	struct word *next;
+};
+
+class List
+{
+	word* head;
+public:
+	List()
 	{
-		if (str[i] == ' ' || str[i] == '\n' || i == Count)
-		{
-			k = 0;
-			for (j = (is+1); j < i; j++)
-			{
-				x[k] = str[j];
-				k++;
-			}
-			x[k] = '\0';
-			ivector.push_back(x);
-			is = i;
-		}
-	i++;
+		head = NULL;
 	}
-	return (ivector);
+	
+	void addNode(char *s, int len)
+	{
+		word *nd = new word;
+		nd -> str = new char[strlen(s)+1];
+		strcpy(nd -> str, s);
+		nd -> next = NULL;
+		
+		if (head == NULL)
+			head = nd;
+		else 
+		{
+			word *current = head;
+			
+			while (current -> next != NULL)
+				current = current -> next;
+			current -> next = nd;
+		}
+	}
+	
+	void CreatList(char *str, int Count)
+	{
+		int i = 0, is = -1, j , k;
+
+		while(i != (Count + 1))
+		{
+			if ( str[i] == ' ' || str[i] == '\n' || i == Count )
+			{
+				char s[i - is];
+				k = 0;
+				for (j = (is + 1); j < i; j++)
+				{
+					s[k] = str[j];
+					k++;
+				}
+				s[k] = '\0';
+				addNode(s, strlen(s));
+				is=i;
+			}
+		i++;
+		}
+	}
+	
+	void print()
+	{
+		word *current = head;	
+		while (current != NULL)
+		{
+			cout << current -> str << endl;
+			current = current -> next;
+		}
+	}
+	
+	word* getnext()
+	{
+		return head -> next;
+	}
+	
+	~List()
+	{			
+		while (head != NULL)
+		{
+			word *current = head -> next;
+			delete [] head -> str;
+			delete head;
+			head = current;
+		}
+	}
+};
+
+
+char *OpRF(char *name)
+{
+	char c;
+	FILE *fp;
+	unsigned int i = 0;
+	for (i = 0; i < strlen(name); i++) 
+		name[i] = name[i + 1];
+	if (name[0] == '\0') 
+		strcpy(name,"index.htm\0");
+	if ((fp = fopen(name, "r")) == NULL)
+	{
+		cout << "Reading error" << endl;
+		return NULL;
+	}
+	fseek(fp, 0, SEEK_END);
+	int l;
+	l = ftell(fp);
+	char *str = new char[l + 1];
+	i = 0;
+	fclose(fp);
+	fp = fopen(name, "r");
+	while((c = fgetc(fp)) != EOF)
+	{
+		str[i] = c;
+		i++;
+	}
+	str[i] = '\0';
+	fclose(fp);
+	return str;
 }
+
 
 class Socket {
 	int numSocket;
@@ -99,43 +193,29 @@ public:
 	
 	void Bind() const
 	{
-		try
+		if (bind(getnum(),(struct sockaddr*) getaddr(), sizeof(*(getaddr())) ) < 0)
 		{
-			if (bind(getnum(),(struct sockaddr*) getaddr(), sizeof(*(getaddr())) ) < 0)
-			{
-				throw 3;
-			}
+			cout << "Can't bind socket" << endl;
 		}
-		
-		catch (int i)
-		{
-			cout << "can't bind socket" << endl;
-		}
+		else 
+			cout << "Bind is ok" << endl;
 	}
 	
 	void Listen() const
 	{
-		try
+		if (listen(getnum(), BACKLOG) < 0)
 		{
-			if (listen(getnum(), BACKLOG) < 0)
-			{
-				throw 4;
-			}
+			cout << "Can't listen socket" <<endl;
 		}
-		
-		catch (int i)
-		{
-			cout << "can't listen socket" <<endl;
-		}
-	}
-
+		else 
+			cout << "Now I'm listening" << endl;
+	};
 };
 
 class Client: public Address {
 	unsigned int len;
 	int sockfd;
 public:
-	friend vector <string> CreatVect(char *str, int Count);
 	Client(): Address() 
 	{
 		len = sizeof(*( getaddr() ));
@@ -143,89 +223,42 @@ public:
 
 	void Accept(Server serv)
 	{
-		try
-		{
-			if ((sockfd = accept (serv.getnum(), (struct sockaddr*) getaddr(), &len)) < 0)
-			{
-				throw 3;
-			}
-		}
-
-		catch (int i)
-		{
-			cout<<"error accepting connection"<<endl;
-		}
+		sockfd = accept (serv.getnum(), (struct sockaddr*) getaddr(), &len);
 	}
 
 	int Request(char* buf, Server serv)
 	{
 		int len, p;
-		try
+		char *st;
+		if ((p = fork()) == -1)
 		{
-			if ((p = fork()) == -1)
+			//cout << "can't make a process" << endl;
+			return 0;
+		}
+		else if (!p)
+		{
+			close(serv.getnum());
+			if ((len = recv(sockfd, buf, BUFLEN-1, 0)) < 0)
 			{
-				throw serv;
+				//cout << "error reading socket" << endl;
+				return 0;
 			}
-			else if (!p)
-			{
-				close(serv.getnum());
-				if ((len = recv(sockfd, buf, BUFLEN-1, 0)) < 0)
-				{
-					throw 12;
-				}
-				buf[BUFLEN] = 0;
-				cout << endl;
-				fprintf(stderr, "Request=%s", buf);
-				if (strncmp(buf, "GET /", 5))
-				{
-					if (send(sockfd, BRSTR, strlen(BRSTR) + 1, 0) != strlen(BRSTR) + 1) 
-					{
-						throw 'a';
-					}
-					shutdown(sockfd, 1);
-					close(sockfd);
-					return 0;
-				}
-				vector <string> ivector = ::CreatVect(buf, BUFLEN);
-				for (unsigned int i = 0; i < ivector.size(); i++)
-					cout << ivector[i] << endl;
-				/*if ((filefd = open(buf+5, O_RDONLY)) < 0)
-				{
-					if (send(sockfd, FNFSTR, strlen(FNFSTR) + 1, 0) != strlen(FNFSTR) + 1)
-					{
-						throw 'a';
-					}
-					shutdown(sockfd, 1);
-					close(sockfd);
-					return 0;
-				}
-				while ((len = read(filefd, &buf, BUFLEN)) > 0)
-				{
-					if (send(sockfd, buf, len, 0) < 0) 
-					{
-						throw 'a';
-					}
-				}
-				close(filefd);*/
-				shutdown(sockfd, 1);
-				close(sockfd);
-				return 0; 
-			}
+			buf[BUFLEN] = 0;
+			cout << "Server request:" << endl;
+			cout << buf << endl;
+			List newl;
+			newl.CreatList(buf, strlen(buf));
+			st = OpRF((newl.getnext()) -> str);
+			cout << "-------------------------------------------------" << endl;
+			send(sockfd,buffer,sizeof(buffer),0);
+			if (st!=NULL)
+				send(sockfd,st,strlen(st),0);
+			cout << "Answer was sent\n";
+			shutdown(sockfd, 0);
 			close(sockfd);
+			return 0; 
 		}
-
-		catch (Server serv)
-		{
-			cout << "can't make a process" << endl;
-		}
-		catch (char x)
-		{
-			cout << "error writing socket" << endl;
-		}
-		catch (int x)
-		{
-			cout << "error reading socket" << endl;
-		}
+		close(sockfd);
 		return 0;
 	}
 
@@ -237,48 +270,8 @@ public:
 }; 
 
 
-
-/*string OpRF(string name)
+int main(int argc, char** argv) 
 {
-	char c;
-	FILE *fp;
-	unsigned int i = 0, l;
-	string str;
-	for (i = 0; i < name.length(); i++) 
-		name[i] = name[i+1];
-	if (name[0] == '\0') 
-		name = "index.htm\0";
-	try
-	{
-		if ((fp = fopen(name,"r")) == NULL)
-		{
-			throw 4;
-		}
-	}
-	
-	catch (int i)
-	{
-		cout << "Reading error" << endl;
-	}
-	
-	fseek(fp, 0, SEEK_END);
-	l = ftell(fp);
-	close(fp);
-	i = 0;
-	fp=fopen(name,"r");
-	while((c = fgetc(fp)) != EOF)
-	{
-		str[i] = c;
-		i++;
-	}
-	str[i]='\0';
-	close(fp);
-	return str;
-}*/
-
-
-
-int main(int argc, char** argv) {
 	if (argc != 2)
 	{
 		cout << "I need PortNumber" << endl;
